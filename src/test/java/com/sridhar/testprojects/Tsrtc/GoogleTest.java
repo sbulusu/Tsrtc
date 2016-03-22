633eb6d515745a44568abea6b1dc8c57df6c4fc4
Sridhar <sridhar249@gmail.com>
2016-03-22 14:58:50 -0600

    added test case code

--- src/test/java/com/sridhar/testprojects/Tsrtc/GoogleTest.java
+++ src/test/java/com/sridhar/testprojects/Tsrtc/GoogleTest.java
@@ -11,6 +11,24 @@ public class GoogleTest {
     @WebTest
     public void test1() {
         Grid.driver().get("http://www.google.com");
+        // Grid.driver().get("http://loner-staging.blacklinegps.com");
+        // try {
+        // Thread.sleep(10000);
+        // } catch (InterruptedException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+        // Grid.driver().navigate().refresh();
+        // Set<Cookie> cookies = Grid.driver().manage().getCookies();
+        // String sessionId = null;
+        // for (Cookie cookie : cookies) {
+        // if (cookie.getName().equals("session")) {
+        // sessionId = cookie.getValue();
+        // }
+        // }
+        // Assert.assertNotNull(sessionId, "Could not find the session.");
+        // String cookie = "session=" + sessionId;
+        // System.out.println(cookie);
     }
 
 }
